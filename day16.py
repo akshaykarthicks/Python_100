3ee54eb2dff341216c4ac59058ad30ed88be24b5
unknown <akshayyourbest153@gmail.com>
2026-06-06 09:14:42 +0530

    first commit

---
+++ day16.py
@@ -0,0 +1,18 @@
+import another
+print(another.a)
+
+from turtle import Turtle,Screen
+ #class =T
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+
+timmy.forward(100)
+timmy.left(120)
+timmy.forward(100)
+
+my_screen= Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
