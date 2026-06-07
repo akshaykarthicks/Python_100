32a5f173aa53b9fbb7045f39e20bbc2d8630be4f
Akshay Karthick <akshaykarthickyourbest153@gmail.com>
2026-06-07 09:37:42 +0530

    sec  commit

--- The Quiz Project/main.py
+++ The Quiz Project/main.py
@@ -3,8 +3,24 @@ class User:
     def __init__(self,userid,username):
         self.id=userid
         self.name=username
+        self.followers=0
+        self.following=0 
+    
+    def follow(self,user):
+        user.followers+=1    
+        self.following+=1
+
+
+
+
 user_1=User("001","AK")
 user_2=User("002","aAK")
 
-print(user_1.id)
-print(user_1.name)
+user_1.follow(user_2) #user 1 follow user 2 
+user_2.follow(user_1)
+
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
